f8341cccfdae50fa6c3b01bc22963ee1e53db310
SubhamChowdhuryRiju <subhamchowdhury1025@gmail.com>
2025-03-25 10:10:07 +0530

    `Added code to hide Streamlit menu and header`

--- app.py
+++ app.py
@@ -15,6 +15,15 @@ st.set_page_config(
     page_icon="🤖",
 )
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 st.title("AI Titans")
 st.subheader("Chat with your documents")
